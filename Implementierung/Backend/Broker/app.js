f9c53846a807468a353df313e903e410fa16bb63
tschmele <Tristan_Gabriel.Schmele@smail.th-koeln.de>
2019-06-11 11:20:21 +0200

    broker set up

---
+++ Implementierung/Backend/Broker/app.js
@@ -0,0 +1,19 @@
+var http = require('http'),
+    faye = require('faye'),
+    server = http.createServer(),
+    bayeux = new faye.NodeAdapter({mount: '/'});
+
+bayeux.attach(server);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("http server listening on port %d.", server.address().port);
+});
+
+var everything = '/*';
+
+bayeux.getClient().subscribe(everything, message => {
+  console.log(`new message @ ${new Date}:`);
+  console.log(message);
+})
+.then(() => {
+  console.log(`subscribed to ${everything}`);
+});
