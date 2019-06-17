c8c60447e5892bf97c5c31888e1bff8f34c7b66c
tschmele <Tristan_Gabriel.Schmele@smail.th-koeln.de>
2019-06-17 11:03:51 +0200

    anzeige in gateway erweitert

--- Implementierung/Backend/Gateway/anzeige.js
+++ Implementierung/Backend/Gateway/anzeige.js
@@ -1,33 +1,125 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    faye = require('faye');
 
+var router = express.Router(),
+    msgClient = new faye.Client('http://localhost:3000');
+
+/*******************************************************************************
+
+*******************************************************************************/
 router.post('/', (req, res) => {
-  if (req.body.autor === undefined) {
+  if (req.body.anfrage === undefined || req.body.autor === undefined
+    || req.body.standort === undefined || req.body.titel === undefined) {
+    res.status(400).end();
+  } else if (req.header('origin') === undefined) {
+    res.statusMessage = 'origin undefined';
     res.status(400).end();
   } else {
-    res.status(201).json('neue anzeige');
+    msgClient.subscribe('/antwort/' + req.header('origin'), message => {
+      if (res.statusMessage)
+        res.statusMessage = message.statusMessage;
+      res.status(message.status).json(message.results);
+    }).then(() => {
+      msgClient.publish('/anzeige/new', {
+        origin : req.header('origin'),
+        action : "post",
+        request : req.body
+      });
+    }
   }
 });
 
+/*******************************************************************************
+
+*******************************************************************************/
 router.get('/', (req, res) => {
-  if (req.query.radius != null && req.query.radius > 0) {
-    res.status(200).json(`alle anzeigen in ${req.query.radius} km umkreis`);
+  if (req.header('origin') === undefined) {
+    res.statusMessage = 'origin undefined';
+    res.status(400).end();
   } else {
-    res.status(200).json('alle anzeigen');
+    msgClient.subscribe('/antwort/' + req.header('origin'), message => {
+      if (res.statusMessage)
+        res.statusMessage = message.statusMessage;
+      res.status(message.status).json(message.results);
+    }).then(() => {
+      if (req.query.radius != null && req.query.radius > 0) {
+        msgClient.publish('/anzeige/alle', {
+          origin : req.header('origin'),
+          action : "get",
+          radius : req.query.radius
+        });
+      } else {
+        msgClient.publish('/anzeige/alle', {
+          origin : req.header('origin'),
+          action : "get"
+        });
+      }
+    });
   }
 });
 
+/*******************************************************************************
+
+*******************************************************************************/
 router.get('/:id', (req, res) => {
-  res.status(200).json(`anzeige mit ID ${req.params.id}`);
+  if (req.header('origin') === undefined) {
+    res.statusMessage = 'origin undefined';
+    res.status(400).end();
+  } else {
+    msgClient.subscribe('/antwort/' + req.header('origin'), message => {
+      if (res.statusMessage)
+        res.statusMessage = message.statusMessage;
+      res.status(message.status).json(message.results);
+    }).then(() => {
+      msgClient.publish('/anzeige/' + req.params.id, {
+        origin : req.header('origin'),
+        action : "get"
+      });
+    });
+  }
 });
 
+/*******************************************************************************
+
+*******************************************************************************/
 router.put('/:id', (req, res) => {
-  res.status(200).json(`aktualisierte anzeige mit ID ${req.params.id}`);
+  if (req.header('origin') === undefined) {
+    res.statusMessage = 'origin undefined';
+    res.status(400).end();
+  } else {
+    msgClient.subscribe('/antwort/' + req.header('origin'), message => {
+      if (res.statusMessage)
+        res.statusMessage = message.statusMessage;
+      res.status(message.status).json(message.results);
+    }).then(() => {
+      msgClient.publish('/anzeige/' + req.params.id, {
+        origin : req.header('origin'),
+        action : "put",
+        request : req.body
+      });
+    });
+  }
 });
 
+/*******************************************************************************
+
+*******************************************************************************/
 router.delete('/:id', (req, res) => {
-  res.statusMessage = `deleted`;
-  res.status(204).end();
+  if (req.header('origin') === undefined) {
+    res.statusMessage = 'origin undefined';
+    res.status(400).end();
+  } else {
+    msgClient.subscribe('/antwort/' + req.header('origin'), message => {
+      if (res.statusMessage)
+        res.statusMessage = message.statusMessage;
+      res.status(message.status).json(message.results);
+    }).then(() => {
+      msg.publish('/anzeige/' + req.params.id, {
+        origin : req.header('origin'),
+        action : "delete"
+      });
+    });
+  }
 });
 
 module.exports = router;
