aa99b6cd27b224e2cd5c5843b619e02e6a733514
tschmele <Tristan_Gabriel.Schmele@smail.th-koeln.de>
2019-06-21 11:07:21 +0200

    added demo variable to test connections without further backend

--- Implementierung/Backend/Broker/app.js
+++ Implementierung/Backend/Broker/app.js
@@ -3,16 +3,27 @@ var http = require('http'),
     server = http.createServer(),
     bayeux = new faye.NodeAdapter({mount: '/'});
 
+var demo = true;
+
 bayeux.attach(server);
 server.listen(process.env.PORT || 3000, () => {
   console.log("http server listening on port %d.", server.address().port);
 });
 
-var everything = '/*';
+var everything = '/**';
 
 bayeux.getClient().subscribe(everything, message => {
   console.log(`new message @ ${new Date}:`);
   console.log(message);
+  if (demo && message.origin != undefined) {
+    bayeux.getClient().publish('/antwort/' + message.origin, {
+      status : 200,
+      statusMessage : 'top brudi',
+      results : {
+        text : 'test erfolgreich'
+      }
+    });
+  }
 })
 .then(() => {
   console.log(`subscribed to ${everything}`);
