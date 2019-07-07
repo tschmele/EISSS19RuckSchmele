fc435e1ac264a6eb32e61fe8a8277b75c2430444
KayRuck <kay.ruck@yahoo.de>
2019-07-07 16:21:45 +0200

    Save Commit

--- Implementierung/FoodInProgress/app/src/main/java/com/example/foodinprogress/ui/display/DisplayFragment.java
+++ Implementierung/FoodInProgress/app/src/main/java/com/example/foodinprogress/ui/display/DisplayFragment.java
@@ -1,35 +1,37 @@
 package com.example.foodinprogress.ui.display;
-
-import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodinprogress.DisplayAdapterListe;
 import com.example.foodinprogress.R;
-import com.example.foodinprogress.dispalydata.AnzeigenInterface;
-import com.example.foodinprogress.dispalydata.Anzeigenliste;
-import com.example.foodinprogress.dispalydata.Example;
-import com.example.foodinprogress.dispalydata.RetroClient;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import java.util.ArrayList;
-import java.util.Objects;
+import com.example.foodinprogress.data.retrofit.AnzeigenInterface;
+import com.example.foodinprogress.data.retrofit.Example;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class DisplayFragment extends Fragment implements DisplayAdapter.OnNoteListener {
+import static com.example.foodinprogress.util.Constants.ERROR;
+import static com.example.foodinprogress.util.Constants.SUCCESS;
 
-    private ArrayList<Example> anzeigenliste;
-    private RecyclerView recyclerView;
+public class DisplayFragment extends Fragment /*implements DisplayAdapter.OnNoteListener */{
 
-    private ProgressDialog pDialog;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,60 +42,101 @@ public class DisplayFragment extends Fragment implements DisplayAdapter.OnNoteLi
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_display2, container, false);
-
-        pDialog = new ProgressDialog(getContext());
-        pDialog.setMessage("Loading Data... Please wait... ");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(false);
-        pDialog.show();
-
-        AnzeigenInterface api = RetroClient.getAnzeigenService();
-        final DisplayFragment current = this;
-
-
-        //Calling JSON
-        Call<Anzeigenliste> call = api.getAnzeigenJSON();
-
-        call.enqueue(new Callback<Anzeigenliste>() {
-
-            @Override
-            public void onResponse(Call<Anzeigenliste> call, Response<Anzeigenliste> response) {
-                pDialog.dismiss();
-
-                if (response.isSuccessful()) {
-                    anzeigenliste = Objects.requireNonNull(response.body()).getExamples();
-                    recyclerView = view.findViewById(R.id.recyclerView_display);
-                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-                    recyclerView.setLayoutManager(layoutManager);
-
-
-                    FloatingActionButton button = view.findViewById(R.id.displayButton);
-                    button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            FragmentTransaction fragmentTransaction = Objects.requireNonNull(getFragmentManager()).beginTransaction();
-                            fragmentTransaction.replace(R.id.fragment_container, new DisplayFragmentEdit()).commit();
-                        }
-                    });
-                    DisplayAdapter adapter = new DisplayAdapter(anzeigenliste, current);
-                    recyclerView.setAdapter(adapter);
-                    recyclerView.setItemAnimator(new DefaultItemAnimator());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Anzeigenliste> call, Throwable t) {
-
-            }
-        });
-
-
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        retroGETALL();
+        buildRecyclerView(view);
         return view;
     }
 
-    @Override
-    public void onItemClick(int position) {
+
+    public DisplayListItem[] generateData() {
+        DisplayListItem[] displayListItems = {
+                new DisplayListItem("Back", R.drawable.ic_back),
+                new DisplayListItem("Row", R.drawable.ic_rows),
+                new DisplayListItem("Pin", R.drawable.ic_pin),
+                new DisplayListItem("Info", R.drawable.ic_information),
+
+                new DisplayListItem("Back", R.drawable.ic_back),
+                new DisplayListItem("Row", R.drawable.ic_rows),
+                new DisplayListItem("Pin", R.drawable.ic_pin),
+                new DisplayListItem("Info", R.drawable.ic_information),
+
+                new DisplayListItem("Back", R.drawable.ic_back),
+                new DisplayListItem("Row", R.drawable.ic_rows),
+                new DisplayListItem("Pin", R.drawable.ic_pin),
+                new DisplayListItem("Info", R.drawable.ic_information)
+        };
+        return displayListItems;
 
     }
+
+    public void buildRecyclerView(View view){
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView_display);
+        recyclerView.setHasFixedSize(true); // For the Performance
+        LinearLayoutManager linearLayout = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayout);
+/*
+        FloatingActionButton button = view.findViewById(R.id.displayButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new DisplayFragmentEdit()).commit();
+            }
+        });
+*/
+        DisplayAdapterListe displayAdapter = new DisplayAdapterListe(generateData());
+        recyclerView.setAdapter(displayAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+
+    }
+/*
+    @Override
+    public void onItemClick(int position) {
+        Log.d(TAG, "onItemClick: work?");
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new DisplayFragmentDetail()).commit();
+    }
+*/
+    public void retroGETALL(){
+        final  TextView textViewResult = null;
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://127.0.0.1:2000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+        AnzeigenInterface api = retrofit.create(AnzeigenInterface.class);
+
+        Call<List<Example>> call = api.getDisplayPosts();
+
+        call.enqueue(new Callback<List<Example>>() {
+            @Override
+            public void onResponse(Call<List<Example>> call, Response<List<Example>> response) {
+                String success = "Funktioniert";
+                Log.d(SUCCESS, success);
+                Toast.makeText(getContext(), success, Toast.LENGTH_SHORT).show();
+
+                if(!response.isSuccessful()){
+                    String result = "Status Code: " + response.code();
+
+                    textViewResult.setText(result);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Example>> call, Throwable t) {
+
+                String error = t.getMessage();
+                Log.d(ERROR, error);
+                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
 }
